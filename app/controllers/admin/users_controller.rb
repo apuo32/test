a7e14c55125c01dab1985ad51b47f190d45e5410
apuo32 <nakataku32@icloud.com>
2023-11-28 03:17:40 +0900

    admin views all finish

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,6 +6,10 @@ class Admin::UsersController < ApplicationController
 
   def new
     @user = User.new # Userモデルのインスタンス作成
+    @departments = Department.all.order(id: "ASC") # idの昇順で取得
+    @evaluators = Evaluator.all.order(id: "ASC") # idの昇順で取得
+    @first_evaluators = User.where(evaluator_id: 1).order(username: :asc) # evaluator_idが1のユーザーのみ取得(1次評価者のみ取得)
+    @second_evaluators = User.where(evaluator_id: 2).order(username: :asc) # evaluator_idが2のユーザーのみ取得(2次評価者のみ取得)
   end
 
   def create
@@ -13,12 +17,20 @@ class Admin::UsersController < ApplicationController
     if @user.save
       redirect_to admin_users_path
     else
+      @departments = Department.all.order(id: "ASC")
+      @evaluators = Evaluator.all.order(id: "ASC")
+      @first_evaluators = User.where(evaluator_id: 1).order(username: :asc)
+      @second_evaluators = User.where(evaluator_id: 2).order(username: :asc)
       render :new
     end
   end
 
   def edit
     @user = User.find(params[:id])
+    @departments = Department.all.order(id: "ASC") # idの昇順で取得
+    @evaluators = Evaluator.all.order(id: "ASC") # idの昇順で取得
+    @first_evaluators = User.where(evaluator_id: 1).order(username: :asc) # evaluator_idが1のユーザーのみ取得(1次評価者のみ取得)
+    @second_evaluators = User.where(evaluator_id: 2).order(username: :asc) # evaluator_idが2のユーザーのみ取得(2次評価者のみ取得)
   end
 
   def update
@@ -42,6 +54,7 @@ class Admin::UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:username,
                                     :password,
+                                    :email,
                                     :employee_code,
                                     :department_id,
                                     :evaluator_id,
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   belongs_to :department
   belongs_to :evaluator
+  belongs_to :first_evaluator, class_name: 'User', foreign_key: 'first_evaluator_id', optional: true
+  belongs_to :second_evaluator, class_name: 'User', foreign_key: 'second_evaluator_id', optional: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
