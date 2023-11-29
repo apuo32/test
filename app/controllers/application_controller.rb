a66fee6f9cbf5865ae2752eae16aae7c968bb7da
apuo32 <nakataku32@icloud.com>
2023-11-29 17:15:03 +0900

    admin login logout finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-
+  before_action :authenticate_user!
+  
+  protected
+  def after_sign_out_path_for(resource_or_scope)
+    "/users/sign_in"
+  end
 end
