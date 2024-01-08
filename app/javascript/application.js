da0a1d2a9737582681380e6ca21854203b87ada9
apuo32 <nakataku32@icloud.com>
2024-01-09 08:55:29 +0900

    imagepreview & drag&drop & modal add

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,6 +9,45 @@ window.$ = jquery
 
 document.addEventListener("turbo:load", function() {
   new TomSelect("#select-kaizen-member", {
-    maxItems: 6
+  });
+
+  const input = document.getElementById('before-images-upload');
+  const previewContainer = document.getElementById('images-preview');
+
+  input.addEventListener('change', (event) => {
+    previewContainer.innerHTML = '';
+    const files = event.target.files;
+
+    Array.from(files).forEach(file => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const img = document.createElement('img');
+        img.src = e.target.result;
+        img.style.maxWidth = '200px'; // 画像の最大幅を設定
+        img.style.maxHeight = '200px'; // 画像の最大高さを設定
+        previewContainer.appendChild(img);
+      };
+      reader.readAsDataURL(file);
+    });
+  });
+
+  const afterInput = document.getElementById('after-images-upload');
+  const afterPreviewContainer = document.getElementById('after-images-preview');
+
+  afterInput.addEventListener('change', (event) => {
+    afterPreviewContainer.innerHTML = '';
+    const files = event.target.files;
+
+    Array.from(files).forEach(file => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const img = document.createElement('img');
+        img.src = e.target.result;
+        img.style.maxWidth = '200px'; // 画像の最大幅を設定
+        img.style.maxHeight = '200px'; // 画像の最大高さを設定
+        afterPreviewContainer.appendChild(img);
+      };
+      reader.readAsDataURL(file);
+    });
   });
 });
